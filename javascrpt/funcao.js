b229b07d617da50195c7d0bd2f7f7c9e2e7a1732
DaviOliveira72 <davioliveiram.faria@gmail.com>
2026-07-24 13:30:59 -0300

    funções

--- javascrpt/funcao.js
+++ javascrpt/funcao.js
@@ -1,5 +1,14 @@
+//peimeiro contato
+/*
 torra()
+
 
 function torra(){
     console.log("Torrando Pão...")
+    InjetarPao()
 }
+function InjetarPao() {
+    console.log("Preparando para injetar o Pão...")
+    console.log("Finalizado")
+}
+*/
